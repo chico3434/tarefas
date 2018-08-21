273f1cc643c095bcadc62d192c36217a41b6e9e1
chico3434 <37938332+chico3434@users.noreply.github.com>
2018-08-21 18:08:40 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
-<?php
-include 'lista.php';
-?>
 <html>
     <head>
         <meta charset="utf-8">
         <title>Tarefas</title>
+        <link rel="stylesheet" href="css/estilo.css">
     </head>
     <body>
-        <?php
-            include 'form.php';
-        ?>
+        <header id="cabecalho">
+            <h1 id="titulo">Tarefas</h1>
+            <h2 id="sub-titulo">Trabalho de desenvolvimento para a internet feito por Francisco Rubens</h2>
+        </header>
+        <section id="corpo">
+            <div id="form">
+                <?php
+                    include 'form.php'; 
+                ?>
+            </div>
+            <div id="lista">
+                <?php
+                    include 'lista.php';
+                ?>
+            </div>
+        </section>
     </body>
 </html>
--- lista.php
+++ lista.php
@@ -5,32 +5,33 @@ $sql = "SELECT * FROM tarefas";
 
 if($resultado = mysqli_query($conexao, $sql)){
     echo "<table>";
-    echo "<tr>
-    <th>id</th>
-    <th>Tarefa</th>
-    <th>Data de Criação</th>
-    <th>Data de Conlusão</th>
-    <th>Concluir</th>
-    <th>Deletar</th>
-    <th>Editar</th>
-    </tr>";
-    while($dado = mysqli_fetch_assoc($resultado)){
-        if($dado['data_conclusao'] == null){
-            $dc = "Não concluído";
-            $concluir = "<a href='concluir.php?id=$dado[id]'>Concluir</a>";
-        } else {
-            $dc = $dado['data_conclusao'];
-            $concluir = "Concluído";
+    $dado = mysqli_fetch_assoc($resultado);
+    if($dado){
+        while($dado){
+            // Formatando o jeito de mostrar a data conslusão e o botão concluir
+            if($dado['data_conclusao'] == null){
+                $data_conclusao = "Não concluído";
+                $concluir = "<a href='concluir.php?id=$dado[id]'>Concluir</a>";
+            } else {
+                $datetime = new DateTime($dado['data_conclusao']);
+                $data_conclusao = $datetime->format("d/m/Y");
+                $concluir = "Concluído";
+            }
+            // Formatando data criacao
+            $data = new DateTime($dado['data_criacao']);
+            $data_criacao = $data->format("d/m/Y");
+            echo "<tr>
+            <td>$data_criacao</td>
+            <td>$dado[tarefa]</td>
+            <td>$data_conclusao</td>
+            <td>$concluir</td>
+            <td><a href='deletar.php?id=$dado[id]'>Deletar</a></td>
+            <td><a href='index.php?id=$dado[id]&value=$dado[tarefa]'>Editar</a></td>
+            </tr>";
+            $dado= mysqli_fetch_assoc($resultado);
         }
-        echo "<tr>
-        <td>$dado[id]</td>
-        <td>$dado[tarefa]</td>
-        <td>$dado[data_criacao]</td>
-        <td>$dc</td>
-        <td>$concluir</td>
-        <td><a href='deletar.php?id=$dado[id]'>Deletar</a></td>
-        <td><a href='index.php?id=$dado[id]&value=$dado[tarefa]'>Editar</a></td>
-        </tr>";
+    }else {
+        echo "<p>Não há tarefas criadas!!<br>Crie uma abaixo!</p>";
     }
     echo "</table>";
     mysqli_close($conexao);
@@ -39,5 +40,4 @@ if($resultado = mysqli_query($conexao, $sql)){
     mysqli_close($conexao);
     die();
 }
-
 ?>
