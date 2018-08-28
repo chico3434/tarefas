3726577a34e9f78cd937df25060234472bb63da1
chico3434 <37938332+chico3434@users.noreply.github.com>
2018-08-28 08:59:39 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -1,15 +1,14 @@
 <?php
 
 $host = 'localhost';
-$usuario = 'francisco';
-$senha = '123';
+$usuario = 'root';
+$senha = '1234';
 $banco = 'tarefas';
 
 $conexao = mysqli_connect($host, $usuario, $senha);
 
 if(!$conexao){
     echo "Erro ao se conectar!" . mysqli_error($conexao);
-    mysqli_close($conexao);
     die();
 } else {
     if(!mysqli_select_db($conexao, $banco)){
